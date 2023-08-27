99a3d54832b77bc2b062b6cb451d5893784462f2
vitran96 <tranuytrieuvi@gmail.com>
2023-08-27 23:25:11 +0700

    update my_server

--- my_server/src/main.rs
+++ my_server/src/main.rs
@@ -3,7 +3,7 @@ struct Server {
 }
 
 impl Server {
-    fn new(addr: &str) -> Self {
+    fn new(addr: String) -> Self {
         Self {
             addr: addr.to_string(),
         }
@@ -15,6 +15,6 @@ impl Server {
 }
 
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
