eed1d054d3b0002a7f720338732822a59138ab27
vitran96 <tranuytrieuvi@gmail.com>
2023-08-28 22:06:26 +0700

    move server part into module

--- my_server/src/main.rs
+++ my_server/src/main.rs
@@ -1,39 +1,47 @@
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr: addr.to_string(),
+mod http {
+    mod method {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            DELETE,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
         }
     }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
+    mod request {
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
+        }
+    }
+}
+
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr: addr.to_string(),
+            }
+        }
+
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
     }
 }
 
 fn main() {
     // Start the server
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = server::Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
