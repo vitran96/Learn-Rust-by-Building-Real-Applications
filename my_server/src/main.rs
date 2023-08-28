9d9df327dd9c0db5c3c6da0aa5e51cdcc6ff0b81
vitran96 <tranuytrieuvi@gmail.com>
2023-08-28 21:45:12 +0700

    add Request struct & Method enum

--- my_server/src/main.rs
+++ my_server/src/main.rs
@@ -1,3 +1,21 @@
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
 struct Server {
     addr: String,
 }
@@ -15,6 +33,7 @@ impl Server {
 }
 
 fn main() {
+    // Start the server
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
