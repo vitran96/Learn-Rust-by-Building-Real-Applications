084de7d817f3b1c43d6e32f34bfa483462eec429
vitran96 <tranuytrieuvi@gmail.com>
2023-08-28 22:18:56 +0700

    move module to separated file

---
+++ my_server/src/http/request.rs
@@ -0,0 +1,7 @@
+use super::Method;
+
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
--- my_server/src/main.rs
+++ my_server/src/main.rs
@@ -1,49 +1,8 @@
-use http::request::Request;
+use http::Request;
 use server::Server;
 
-mod http {
-    pub mod method {
-        pub enum Method {
-            GET,
-            POST,
-            PUT,
-            DELETE,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
-
-    pub mod request {
-        use super::method::Method;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: Method,
-        }
-    }
-}
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self {
-                addr: addr.to_string(),
-            }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
-        }
-    }
-}
+mod http;
+mod server;
 
 fn main() {
     // Start the server
