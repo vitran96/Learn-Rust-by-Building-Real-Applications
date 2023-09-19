adc05658790c47ea1b087f6ea680f823556fe5a2
vitran96 <tranuytrieuvi@gmail.com>
2023-09-19 21:58:51 +0700

    move response send to it's own module

--- my_server/src/http/response.rs
+++ my_server/src/http/response.rs
@@ -1,4 +1,8 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    fmt::{Display, Formatter},
+    io::Write,
+    net::TcpStream,
+};
 
 use super::StatusCode;
 
@@ -15,16 +19,31 @@ impl Response {
             body: body,
         }
     }
-}
 
-impl Display for Response {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+    pub fn send(&self, stream: &mut TcpStream) -> std::io::Result<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+
         write!(
-            f,
+            stream,
             "HTTP/1.1 {} {}\r\n\r\n{}",
             self.status_code,
             self.status_code.reason_phrase(),
-            self.body.clone().unwrap_or(String::from(""))
+            body
         )
     }
 }
+
+// impl Display for Response {
+//     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+//         write!(
+//             f,
+//             "HTTP/1.1 {} {}\r\n\r\n{}",
+//             self.status_code,
+//             self.status_code.reason_phrase(),
+//             self.body.clone().unwrap_or(String::from(""))
+//         )
+//     }
+// }
--- my_server/src/server.rs
+++ my_server/src/server.rs
@@ -3,7 +3,7 @@
 // and also sub-item
 // #![allow(dead_code)]
 
-use crate::http::{response, Response, StatusCode};
+use crate::http::{Response, StatusCode};
 // Must go from root. We use "crate::" instead of "super::"
 use crate::http::request::Request;
 use std::convert::TryFrom;
@@ -40,7 +40,7 @@ impl Server {
                             // must convert to &[u8; 1024]
                             // Request::try_from(&buf[..]);
                             // let res: &Result<Request, _> = &buf.try_into();
-                            match Request::try_from(&buf[..]) {
+                            let result = match Request::try_from(&buf[..]) {
                                 Ok(request) => {
                                     // buf is borrowed by request.
                                     // so we can't use buf anymore
@@ -50,13 +50,23 @@ impl Server {
                                     // let response =
                                     //     Response::new(crate::http::StatusCode::NotFound, None);
 
-                                    let response = Response::new(
+                                    Response::new(
                                         StatusCode::Ok,
                                         Some("<h1>Hello world!</h1>".to_string()),
-                                    );
-                                    write!(stream, "{}", response);
+                                    )
+
+                                    // Doing this, we will create new allocation
+                                    // write!(stream, "{}", response);
                                 }
-                                Err(e) => println!("Failed to parse a request:  {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse a request:  {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            // Send and Check error here
+                            if let Err(e) = result.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
