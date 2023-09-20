ca2dbca333f462abec59475e8b50f405b6659980
vitran96 <tranuytrieuvi@gmail.com>
2023-09-20 23:16:52 +0700

    add public path to server

--- my_server/src/main.rs
+++ my_server/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use server::Server;
 use website_handler::WebsiteHandler;
 
@@ -6,7 +8,12 @@ mod server;
 mod website_handler;
 
 fn main() {
+    // CARGO_MANIFEST_DIR is an environment variable that points to the root of the project
+    let default_public_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_public_path);
+    println!("Using public path: {}", public_path);
+
     // Start the server
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
