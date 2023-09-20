a029c0d3bd482e17cc23e8179ceee03f2f8478a3
vitran96 <tranuytrieuvi@gmail.com>
2023-09-20 22:45:01 +0700

    handle routing

--- my_server/src/website_handler.rs
+++ my_server/src/website_handler.rs
@@ -1,5 +1,5 @@
 use crate::{
-    http::{Request, Response, StatusCode},
+    http::{Method, Request, Response, StatusCode},
     server::Handler,
 };
 
@@ -7,6 +7,16 @@ pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Hello world!</h1>".to_string()))
+        // Response::new(StatusCode::Ok, Some("<h1>Hello world!</h1>".to_string()))
+        match request.method() {
+            // Handle Routing for method
+            // Handling GET method
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Hello world!</h1>".to_string())),
+                "/welcome" => Response::new(StatusCode::Ok, Some("<h1>Welcome!</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
